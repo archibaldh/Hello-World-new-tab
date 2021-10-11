e8c894195e1ef0a75964902d161f3b9363585801
gitgub12 <46924671+gitgub12@users.noreply.github.com>
2021-10-12 08:23:54 +1100

    Beta release 0.2.0! Major UI Changes: Repositioned clock Added a settings button (not functional yet) Added quick links!

--- Hello World Extension/main.js
+++ Hello World Extension/main.js
@@ -36,3 +36,18 @@ const clockElement = document.querySelector(".clock");
 const clockObject = new DigitalClock(clockElement);
 
 clockObject.start();
+
+let modalBtn = document.getElementById("modal-btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+    modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+    modal.style.display = "none"
+}
+window.onclick = function(e){
+    if(e.target == modal){
+        modal.style.display = "none"
+    }
+}
